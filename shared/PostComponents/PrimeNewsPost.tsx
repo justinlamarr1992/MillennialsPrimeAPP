65901714e9203c27e9555cfc758c67771b0b5c32
Michael Stelly <michael.stelly@equaldefenseresearch.com>
2025-10-23 12:01:09 -0500

    Fix TypeScript errors in PrimeNewsPost and PreviewCard
    PrimeNewsPost (13 errors fixed):
    - Added PrimeNewsPostProps interface
    - Removed unused expo-av import
    
    PreviewCard (9 errors fixed):
    - Added PreviewCardProps interface
    - Switched to COLORS from theme colors
    - Removed non-existent vertMargin style

--- shared/PostComponents/PrimeNewsPost.tsx
+++ shared/PostComponents/PrimeNewsPost.tsx
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
 import { Text } from "react-native";
 import { WebView } from "react-native-webview";
-import { Video, ResizeMode } from "expo-av";
 import { LinearGradient } from "expo-linear-gradient";
 import UserInfo from "./UserInfo";
 import { globalStyles } from "@/constants/global";
 import LikeComment from "../LikeComment";
+
+interface PrimeNewsPostProps {
+  guid: string;
+  dateUploaded: string;
+  videoLibraryId: string;
+  name: string;
+  time: string;
+  url: string;
+  title: string;
+  description: string;
+  prime: boolean;
+  admin: boolean;
+  libraryId: string;
+  videoId: string;
+}
 
 export default function PrimeNewsPost({
   guid,
@@ -20,7 +34,7 @@ export default function PrimeNewsPost({
   admin,
   libraryId,
   videoId,
-}) {
+}: PrimeNewsPostProps) {
   // Determins if the post will be red or gold
 
   return (
